caf6a744d13e50037d264f3d3fb2d2b4a8c052f9
Marco <ma.angiusmarco@gmail.com>
2016-03-09 14:44:01 +0100

    XML layout and class declaration v2

--- app/src/main/java/com/example/android/climbthemountain/Login.java
+++ app/src/main/java/com/example/android/climbthemountain/Login.java
@@ -8,6 +8,6 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_and_register_01);
     }
 }
