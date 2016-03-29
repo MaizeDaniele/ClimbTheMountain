3da82d50afe114cf8043087b3e17aef92e938804
Marco <mr.angiusmarco@gmail.com>
2016-03-29 22:56:39 +0200

    Status:
    -manifest change to fit each activity's purpose
    -registered each element on every activity
    -added link to activity
    -debug stuff

--- app/src/main/java/com/example/android/climbthemountain/Login.java
+++ app/src/main/java/com/example/android/climbthemountain/Login.java
@@ -1,13 +1,60 @@
 package com.example.android.climbthemountain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    // elements
+    Button btLogin;
+    EditText etUsername;
+    EditText etPassword;
+    TextView tvNewUser;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_and_register_01);
+
+
+
+        // elements instance
+        btLogin = (Button) findViewById(R.id.btLogin_login);
+        etUsername = (EditText) findViewById(R.id.etLogin_username);
+        etPassword = (EditText) findViewById(R.id.etLogin_password);
+        tvNewUser = (TextView) findViewById(R.id.tvLogin_newUser);
+
+
+
+        // login button
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               authentication();
+            }
+        });
+
+        // new user
+        tvNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterAccountData.class));
+            }
+        });
+
+
+    }
+
+    // authentication method to verify credential
+    private void authentication() {
+
     }
 }
--- app/src/main/java/com/example/android/climbthemountain/RegisterAccountData.java
+++ app/src/main/java/com/example/android/climbthemountain/RegisterAccountData.java
@@ -1,13 +1,73 @@
 package com.example.android.climbthemountain;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class RegisterAccountData extends AppCompatActivity {
+
+    // elements
+    EditText etUsername;
+    EditText etPassword;
+    EditText etPasswordRepeat;
+    EditText etName;
+    EditText etSurname;
+
+
+
+    // toolbar
+    Toolbar tbRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_account_02);
+
+        // elements instance
+        etName = (EditText) findViewById(R.id.etReg_name);
+        etSurname = (EditText) findViewById(R.id.etReg_surname);
+        etUsername = (EditText) findViewById(R.id.etReg_username);
+        etPassword = (EditText) findViewById(R.id.etReg_pass);
+        etPasswordRepeat = (EditText) findViewById(R.id.etReg_passRep);
+
+        // toolbar registration
+        tbRegistration = (Toolbar) findViewById(R.id.tbReg_toolbar);
+        setSupportActionBar(tbRegistration);
+
+
+
     }
+
+    // inflate the menu for App Bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.register_account_data, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    // manage menu's items
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        saveRegisterData();
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    // method to save user details
+    private void saveRegisterData(){
+        startActivity(new Intent(getApplicationContext(), RegisterSessionData.class));
+    }
+
+
 }
+
